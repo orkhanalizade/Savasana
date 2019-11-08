e8d114fb9145629dfad229000ee3f5a6d1bf8dda
Orkhan Alizada <orkhan.alizada@asanarebel.com>
2019-11-08 11:18:07 +0100

    Initial Commit

---
+++ Savasana/Savasana.h
@@ -0,0 +1,19 @@
+//
+//  Savasana.h
+//  Savasana
+//
+//  Created by Orkhan Alizada on 08.11.19.
+//  Copyright © 2019 Orkhan Alizada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Savasana.
+FOUNDATION_EXPORT double SavasanaVersionNumber;
+
+//! Project version string for Savasana.
+FOUNDATION_EXPORT const unsigned char SavasanaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Savasana/PublicHeader.h>
+
+
